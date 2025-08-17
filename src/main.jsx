b2a34c799c806ff3fd0047d39511982289db3b13
ingcivilarqricardomorales-bit <ing.civil.arq.ricardo.morales@gmail.com>
2025-08-17 17:28:42 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import Website from './App.jsx'
+import './index.css'
+createRoot(document.getElementById('root')).render(<Website />)
